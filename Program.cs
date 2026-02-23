e0b2c504c6b0342827d09e7a39b136e58243a7c1
seanrabillasremonde-cmyk <seanrabillas.remonde@dbtc-cebu.edu.ph>
2026-02-23 15:25:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Data2;
+
+namespace Data
+{
+    class program
+    {
+
+        public static void Main()
+        {
+            List<Product> products = new List<Product>();
+            products.AddRange([new Product()
+            {
+                Name = "Piatos",
+                Id = 1,
+                Price = (decimal)10.50,
+                CreatedDate = DateTime.Now
+            },
+
+            new Product()
+            {
+                Name = "Nova",
+                Id = 2,
+                Price = (decimal)10.50,
+                CreatedDate = DateTime.Now
+            },
+            new Product()
+            {
+                Name = "Nova",
+                Id = 2,
+                Price = (decimal)10.50,
+                CreatedDate = DateTime.Now
+            
+            
+            }]);
+
+            foreach (var product in products) 
+            {
+                Console.WriteLine($"Id: {product.Id}\n" +
+                                 $"Name:{product.Name}\n" +
+                                 $"Price:{product.Price}\n" +
+                                $" CreatedDate:{product.CreatedDate.ToShortDateString()}");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
