9a8ea0d7d63a17dd80e8812a121cab0bfee7b3f2
seanrabillasremonde-cmyk <seanrabillas.remonde@dbtc-cebu.edu.ph>
2026-02-23 15:20:55 +0700

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -2,15 +2,15 @@
 
 namespace Data2
 {
-    class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public DateTime CreatedDate { get; set; }
-
-
-
+     class Product
+     {
+         public int Id { get; set; }  
+         public string Name { get; set; }
+         public decimal Price { get; set; }
+         public DateTime CreatedDate { get; set; } 
+    
+    
+    
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,64 +1,36 @@
-﻿using System;
+﻿
+using System;
 using Data2;
+
 
 namespace Data
 {
+
     class program
     {
 
         public static void Main()
         {
-            List<Product> products = new List<Product>();
-            products.AddRange([new Product()
+            Product product = new Product();
             {
-                Name = "Piatos",
-                Id = 1,
-                Price = (decimal)10.50,
-                CreatedDate = DateTime.Now
-            },
+                product.Id = 5;
+                product.Name = "Kilaton";
+                product.Price = (decimal)1.50;
+                product.CreatedDate = DateTime.Now;
 
-            new Product()
-            {
-                Name = "Nova",
-                Id = 2,
-                Price = (decimal)10.50,
-                CreatedDate = DateTime.Now
-            },
-            new Product()
-            {
-                Name = "Nova",
-                Id = 2,
-                Price = (decimal)10.50,
-                CreatedDate = DateTime.Now
-            
-            
-            }]);
 
-            foreach (var product in products) 
-            {
                 Console.WriteLine($"Id: {product.Id}\n" +
                                  $"Name:{product.Name}\n" +
                                  $"Price:{product.Price}\n" +
                                 $" CreatedDate:{product.CreatedDate.ToShortDateString()}");
-            }
 
-
-
-
-
-
-
-
-
-
-
-
-
+            }      
+            
         }
+    
     }
+
+
+
 }
-
-
-
-
 
