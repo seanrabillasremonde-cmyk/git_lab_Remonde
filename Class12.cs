b1b2a929fe516ed0a5ef0774d15efa2ad7a1c264
seanrabillasremonde-cmyk <seanrabillas.remonde@dbtc-cebu.edu.ph>
2026-02-23 15:23:37 +0700

    Add files via upload

---
+++ Class12.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Data2
+{
+     class Product
+     {
+         public int Id { get; set; }  
+         public string Name { get; set; }
+         public decimal Price { get; set; }
+         public DateTime CreatedDate { get; set; } 
+    
+    
+    
+    }
+
+}
---
+++ program1.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using Data2;
+
+
+namespace Data
+{
+
+    class program
+    {
+
+        public static void Main()
+        {
+            Product product = new Product();
+            {
+                product.Id = 5;
+                product.Name = "Kilaton";
+                product.Price = (decimal)1.50;
+                product.CreatedDate = DateTime.Now;
+
+
+                Console.WriteLine($"Id: {product.Id}\n" +
+                                 $"Name:{product.Name}\n" +
+                                 $"Price:{product.Price}\n" +
+                                $" CreatedDate:{product.CreatedDate.ToShortDateString()}");
+
+            }      
+            
+        }
+    
+    }
+
+
+
+}
+
